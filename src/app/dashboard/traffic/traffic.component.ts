317ec86406abb7016cee830d27ee09b0d9a1b342
pankhur <negipankhur@gmail.com>
2025-08-04 18:49:46 +0530

    extended built-in elements with custom components

---
+++ src/app/dashboard/traffic/traffic.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-traffic',
+  standalone: true,
+  imports: [],
+  templateUrl: './traffic.component.html',
+  styleUrl: './traffic.component.css'
+})
+export class TrafficComponent {
+  dummyTrafficData() {
+
+  }
+
+  maxTraffic() {
+
+  }
+}
